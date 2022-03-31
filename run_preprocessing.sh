3d6553966a9a01ad021f1f8c936a8d363150ae94
sim-so <96299403+sim-so@users.noreply.github.com>
2022-03-31 22:09:53 +0900

    Delete run_preprocessing.sh

--- run_preprocessing.sh
+++
@@ -1,7 +0,0 @@
-DIR=./data/
-TRAIN_FN=train.tsv
-TEST_FN=test.tsv
-
-shuf {DIR}{TRAIN_FN} > {DIR}full_train.shuf.tsv
-tail -n 500 {DIR}full_train.shuf.tsv > {DIR}valid.shuf.tsv
-head -n 2494 {DIR}full_train.shuf.tsv > {DIR}.train.shuf.tsv
